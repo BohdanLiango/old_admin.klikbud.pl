ee08f1f01814bad986ab7bdd657203a6eb943c78
BohdanLiango <bohdan.liango@gmail.com>
2021-06-27 00:44:26 +0200

    nn

--- resources/views/livewire/warehouses/tools/block/index/tools.blade.php
+++ resources/views/livewire/warehouses/tools/block/index/tools.blade.php
@@ -15,7 +15,7 @@
                         <a href="{{ route('warehouses.tools.one', $tool->slug) }}" class="btn font-weight-bolder btn-sm btn-info mr-1"><i class="fa fa-eye"></i></a>
                         @if($collect_items_cart->contains($tool->id) or $tool->box_id !== NULL)
                             @else
-                        <a href="#" class="btn font-weight-bolder btn-sm btn-light-success mr-1" wire:click.prevent.defer="addToolToCart({{ $tool->id }}, {{ json_encode($tool->box_id) }})"><i class="fa fa-cart-plus"></i></a>
+                        <a href="#" class="btn font-weight-bolder btn-sm btn-light-success mr-1" wire:click.prevent.lazy="addToolToCart({{ $tool->id }}, {{ json_encode($tool->box_id) }})"><i class="fa fa-cart-plus"></i></a>
                         @endif
 
                         @if($tool->is_box === 1)
